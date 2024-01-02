90aaf74b4a167750adf2ef41f7a347e2e7ef6d48
Chanu Perera <78588327+ChanuPerera@users.noreply.github.com>
2024-01-02 11:02:00 +0530

    Commit ID #5

--- src/components/DoubleNav.jsx
+++ src/components/DoubleNav.jsx
@@ -14,79 +14,93 @@ import {
 import Logo from "../assets/images/logo.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import * as Icons from "@mui/icons-material";
+import classnames from "classnames";
 
 const DoubleNavbar = () => {
- 
-
   const links = [
     {
       name: "Skills",
       link: "Skills",
+      icon: "AllInclusive",
     },
     {
       name: "Blog",
       link: "Blog",
+      icon: "Book",
     },
     {
       name: "Study",
       link: "Study",
+      icon: "School",
     },
   ];
 
   const bottomLinks = [
     {
       title: "Solutions",
+      icon: "EmojiObjects",
       sublinks: [
         {
           name: "Skill",
           description: "dwad awdad ada",
+          icon: "Bolt",
         },
         {
           name: "Courses",
           description: "dwad awdad ada",
+          icon: "BorderColor",
         },
         {
           name: "Technologies",
           description: "dwad awdad ada",
+          icon: "Hub",
         },
       ],
     },
     {
       title: "Products",
+      icon: "Dns",
       sublinks: [
         {
-          name: "Products Skill",
+          name: "Product Skill",
           description: "dwad awdad ada",
+          icon: "Bolt",
         },
         {
-          name: "Products Courses",
+          name: "Product Courses",
           description: "dwad awdad ada",
+          icon: "BorderColor",
         },
         {
-          name: "Products Technologies",
+          name: "Product Technologies",
           description: "dwad awdad ada",
+          icon: "Hub",
         },
       ],
     },
     {
       title: "Technologies",
+      icon: "OnlinePrediction",
       sublinks: [
         {
           name: "Technical Skill",
           description: "dwad awdad ada",
+          icon: "Bolt",
         },
         {
-          name: "Technical Courses",
+          name: "Tech Courses",
           description: "dwad awdad ada",
+          icon: "BorderColor",
         },
         {
-          name: "Technical Technologies",
-          description: "dwad awdad adaddddd",
+          name: "Tech Stack",
+          description: "dwad awdad ada",
+          icon: "Hub",
         },
       ],
     },
   ];
-
 
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
@@ -101,7 +115,6 @@ const DoubleNavbar = () => {
     }
   };
 
-
   useEffect(() => {
     const handleScroll = () => {
       if (isDropdownOpen) {
@@ -109,39 +122,41 @@ const DoubleNavbar = () => {
       }
     };
 
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, [isDropdownOpen]);
-
-
 
   const renderDropdownContent = () => {
     if (!selectedLink) {
       return null;
     }
 
-    const selectedLinkData = bottomLinks.find((link) => link.title === selectedLink);
+    const selectedLinkData = bottomLinks.find(
+      (link) => link.title === selectedLink
+    );
 
     if (!selectedLinkData) {
       return null;
     }
 
-
-
-    
-
     return (
-      <>
+      <div className="container">
         <h5>{selectedLinkData.title} Solutions</h5>
         <hr />
-        <div className="dropdown-body justify-content-start">
+        <div className="dropdown-body justify-content-start mx-auto">
           {selectedLinkData.sublinks.map((sublink, subindex) => (
             <div key={subindex} className="d-flex flex-col">
               <div className="nav-link d-flex flex-row p-1 justify-content-start bg-white mb-1 align-items-center col-md-4 col-12 rounded">
-                <div className="icon-wrapper rounded-circle mr-2"></div>
+                <div className="icon-wrapper rounded-circle mr-2 d-flex justify-content-center align-items-center">
+                  <span className="Icon-Wrapper text-white">
+                    {Icons[sublink.icon] &&
+                      React.createElement(Icons[sublink.icon])}
+                  </span>
+                </div>
+
                 <div className="info d-flex flex-column ">
                   <h6>
                     <strong>{sublink.name}</strong>
@@ -152,34 +167,38 @@ const DoubleNavbar = () => {
             </div>
           ))}
         </div>
-      </>
+      </div>
     );
   };
-  
+
   return (
     <>
       <Navbar expand="lg" fluid className="top-nav">
-        <Navbar.Toggle aria-controls="first-row" >
-        <FontAwesomeIcon icon={faBars} />
+        <Navbar.Toggle aria-controls="first-row">
+          <FontAwesomeIcon icon={faBars} />
         </Navbar.Toggle>
         <Navbar.Collapse id="first-row" className="justify-content-center ">
           <Nav className="nav-body mx-auto justify-content-end">
             {links.map((linkitem, index) => (
               <div key={index}>
-                <Nav.Link href="#" className="nav-link">
+                <li href="#" className="nav-link">
+                  <span className="Icon-Wrapper mr-2">
+                    {Icons[linkitem.icon] &&
+                      React.createElement(Icons[linkitem.icon])}
+                  </span>
                   {linkitem.name}
-                </Nav.Link>
+                </li>
               </div>
             ))}
-            <Button variant="outline-light" className="login-btn">Login</Button>
+            <Button variant="outline-light" className="login-btn">
+              Login
+            </Button>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
 
-      <Navbar expand="lg" fluid className="bottom-nav p-md-0" >
+      <Navbar expand="lg" fluid className="bottom-nav p-md-0">
         <div className="nav-body mx-auto justify-content-start">
-        
-        
           <div
             id="second-row"
             className="d-flex flex-row justify-content-between align-items-center"
@@ -195,74 +214,51 @@ const DoubleNavbar = () => {
                 {bottomLinks.map((linkitem, index) => (
                   <div key={index} className="d-flex flex-row">
                     <li
-                      className="nav-link"
+                      className={classnames("nav-link", {
+                        "selected-link": selectedLink === linkitem.title,
+                      })}
                       onClick={() => toggleDropdown(linkitem.title)}
                     >
+                      <span className="Icon-Wrapper mr-2">
+                        {Icons[linkitem.icon] &&
+                          React.createElement(Icons[linkitem.icon])}
+                      </span>
                       {linkitem.title}
                     </li>
                   </div>
                 ))}
-                 <button type="button" className="btn enroll-btn d-md-none block rounded-pill ">Enroll now</button>
+                <button
+                  type="button"
+                  className="btn enroll-btn d-md-none block rounded-pill "
+                >
+                  Enroll
+                </button>
               </Nav>
             </div>
-            
+
             <div className="d-md-block d-none">
-              <button type="button" className="btn enroll-btn rounded-pill py-2 px-4">Enroll now</button>
+              <button
+                type="button"
+                className="btn enroll-btn rounded-pill py-2 px-4"
+              >
+                Enroll now
+              </button>
             </div>
           </div>
         </div>
       </Navbar>
 
-
-<div className="container drop-container ">
-{/* {isDropdownOpen && (
-<Container className={`mx-auto  p-5 justify-content-start rounded-bottom dropdown-body-wrapper ${isDropdownOpen ? '' : 'slide-up'}`}>
-       {bottomLinks.map((link,index) => (
- <div className="" key={index}>
-<h5>{link.title} Solutions</h5>
-          <hr></hr>
-          <div className="dropdown-body justify-content-start">
-            {link.sublinks.map((sublink, subindex) => (
-                <div key={subindex} className="d-flex flex-col">
-                  <div className="nav-link d-flex flex-row p-1 justify-content-start bg-white mb-1 align-items-center col-md-4 col-12 rounded">
-                    <div className="icon-wrapper rounded-circle mr-2"></div>
-                    <div className="info d-flex flex-column ">
-                      <h6>
-                        <strong>{sublink.name}</strong>
-                      </h6>
-                      <p>{sublink.description}</p>
-                    </div>
-                  </div>
-                </div>
-              ))}
-          </div>
- </div>
-
-       ))}
-      
-          
-        </Container>
-)} */}
-
-
-
-
-
-
-
-
-
-
-
-{isDropdownOpen && (
-          <Container className={`mx-auto p-5 justify-content-start rounded-bottom dropdown-body-wrapper ${isDropdownOpen ? '' : 'slide-up'}`}>
+      <div className="container drop-container mx-auto">
+        {isDropdownOpen && (
+          <Container
+            className={`mx-auto p-5 justify-content-start rounded-bottom dropdown-body-wrapper ${
+              isDropdownOpen ? "" : "slide-up"
+            }`}
+          >
             {renderDropdownContent()}
           </Container>
         )}
-
-</div>
-      
-      
+      </div>
     </>
   );
 };
--- src/routes/Landing.jsx
+++ src/routes/Landing.jsx
@@ -1,9 +1,9 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import DoubleNavbar from "../components/DoubleNav";
 import Hero from "../assets/images/hero2.png";
 
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 import Fb from "../assets/images/fb.png";
 import Gc from "../assets/images/gc.png";
@@ -11,49 +11,60 @@ import Ms from "../assets/images/ms.png";
 import Oc from "../assets/images/Oc.png";
 import AWS from "../assets/images/aws.png";
 
+import * as Icons from "@mui/icons-material";
+
 function Landing() {
-
-
   const brandlogo = [
     {
       name: "Facebook",
-      image:Fb
+      image: Fb,
     },
     {
       name: "Google Cloud",
-      image:Gc
+      image: Gc,
     },
     {
       name: "Microsoft",
-      image:Ms
+      image: Ms,
     },
 
     {
       name: "Oracle",
-      image:Oc
+      image: Oc,
     },
     {
       name: "AWS",
-      image:AWS
+      image: AWS,
     },
-  ]
+  ];
 
-
-
-
-
-
-
-
-
-
+  const services = [
+    {
+      title: "Software Product Development",
+      icon: "DeveloperMode",
+      des: "We are a software development company delivering software architecture services to assist clients in designing and building software products or integrating new tools into the existing software systems.",
+    },
+    {
+      title: "Internet of Things",
+      icon: "Hub",
+      des: "We build software for IoT, Smart Home and Robotics devices: from the architecture through embedded & middleware to interfaces & apps.",
+    },
+    {
+      title: "Cloud & DevOps Solutions",
+      icon: "CloudDone",
+      des: "Helping businesses leverage cloud technology, including setting up a cloud infrastructure, migrating applications to the cloud, developing cloud-based applications, and supporting cloud platforms.",
+    },
+  ];
 
   return (
     <div className="App">
       <DoubleNavbar />
       <header className="App-header text-white">
         <div className="container p-5 justify-content-center align-items-center">
-          <h1>Pluralsight enables you <br/>to develop technical expertise.</h1>
+          <h1>
+            Pluralsight enables you <br />
+            to develop technical expertise.
+          </h1>
           <p>
             Cultivate essential tech competencies, reduce project timelines,
             foster more content tech teams, and turn your objectives into
@@ -76,126 +87,98 @@ function Landing() {
       <section id="features" className="section text-white">
         <div className="container">
           <h2>Our Services</h2>
+
           <div className="row">
-            <div className="col-md-4 rounded-md">
-              <div className="feature-card ">
-                <h3>Feature 1</h3>
-                <p>Description of feature 1.</p>
-              </div>
-            </div>
+            {services.map((service, index) => (
+              <div className="col-md-4 d-flex" key={index}>
+                <div className="feature-card bg-dark rounded justify-center">
+                  <div className="service-icon-wrapper rounded-circle mx-auto mb-2 d-flex justify-content-center align-items-center">
+                    <span className="Icon-Wrapper text-white">
+                      {Icons[service.icon] &&
+                        React.createElement(Icons[service.icon])}
+                    </span>
+                  </div>
 
-            <div className="col-md-4">
-              <div className="feature-card">
-                <h3>Feature 2</h3>
-                <p>Description of feature 2.</p>
+                  <h3>{service.title}</h3>
+                  <p>{service.des}</p>
+                </div>
               </div>
-            </div>
-
-            <div className="col-md-4">
-              <div className="feature-card">
-                <h3>Feature 3</h3>
-                <p>Description of feature 3.</p>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </section>
 
       <section id="cta" className="section text-white">
         <div className="container">
-  
-
-
-<Carousel
-  additionalTransfrom={0}
-  arrows
-  autoPlaySpeed={3000}
-  centerMode
-  className=""
-  containerClass="container"
-  dotListClass=""
-  draggable
-  focusOnSelect={false}
-  infinite
-  itemClass=""
-  keyBoardControl
-  minimumTouchDrag={80}
-  pauseOnHover
-  renderArrowsWhenDisabled={false}
-  renderButtonGroupOutside={false}
-  renderDotsOutside={false}
-  responsive={{
-    desktop: {
-      breakpoint: {
-        max: 3000,
-        min: 1024
-      },
-      items: 3,
-      partialVisibilityGutter: 40
-    },
-    mobile: {
-      breakpoint: {
-        max: 464,
-        min: 0
-      },
-      items: 1,
-      partialVisibilityGutter: 30
-    },
-    tablet: {
-      breakpoint: {
-        max: 1024,
-        min: 464
-      },
-      items: 2,
-      partialVisibilityGutter: 30
-    }
-  }}
-  rewind={false}
-  rewindWithAnimation={false}
-  rtl={false}
-  shouldResetAutoplay
-  showDots={false}
-  sliderClass=""
-  slidesToSlide={1}
-  swipeable
->
-
-  {brandlogo.map((logo,index) => (
-    <div className="w-100" key={index}>
-<div className="col-md-8 col-4 brand-logo">
-          <img src={logo.image} alt={logo.name}/>
-        </div>
-</div>
-        
-
-  ))}
-  
-
-          
-          
-
-          
-        </Carousel>
-
-
-
-
-
-        </div>
-      </section>
-
-      <section id="contact" className="section">
-        <div className="container">
-          <h2>Contact Us</h2>
+          <Carousel
+            additionalTransfrom={0}
+            arrows
+            autoPlaySpeed={3000}
+            centerMode
+            className=""
+            containerClass="container"
+            dotListClass=""
+            draggable
+            focusOnSelect={false}
+            infinite
+            itemClass=""
+            keyBoardControl
+            minimumTouchDrag={80}
+            pauseOnHover
+            renderArrowsWhenDisabled={false}
+            renderButtonGroupOutside={false}
+            renderDotsOutside={false}
+            responsive={{
+              desktop: {
+                breakpoint: {
+                  max: 3000,
+                  min: 1024,
+                },
+                items: 3,
+                partialVisibilityGutter: 40,
+              },
+              mobile: {
+                breakpoint: {
+                  max: 464,
+                  min: 0,
+                },
+                items: 1,
+                partialVisibilityGutter: 30,
+              },
+              tablet: {
+                breakpoint: {
+                  max: 1024,
+                  min: 464,
+                },
+                items: 2,
+                partialVisibilityGutter: 30,
+              },
+            }}
+            rewind={false}
+            rewindWithAnimation={false}
+            rtl={false}
+            shouldResetAutoplay
+            showDots={false}
+            sliderClass=""
+            slidesToSlide={1}
+            swipeable
+          >
+            {brandlogo.map((logo, index) => (
+              <div className="w-100" key={index}>
+                <div className="col-md-8 col-12 brand-logo">
+                  <img src={logo.image} alt={logo.name} />
+                </div>
+              </div>
+            ))}
+          </Carousel>
         </div>
       </section>
 
       <footer className="footer">
-        <div className="container">
+        <div className="container text-white">
           <p>&copy; 2024 Your Company. All rights reserved.</p>
         </div>
       </footer>
-
     </div>
   );
 }
